387ed6fe06546d25ac738c3a9838ac71271f8da4
ovextemeer <ovextemeer@protonmail.com>
2024-08-22 11:06:58 +0700

    Completed project

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+// import libraries
+const multer = require('multer');
+const upload = multer();
+// import libraries
+
 var app = express();
 
 app.use(cors());
@@ -11,8 +16,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// start building project here
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  if (req.file !== undefined) {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  } else {
+    console.log("No file chosen");
+  }
+});
+// end building project here
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
